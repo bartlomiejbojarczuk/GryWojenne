42dd01709888fabe485d54868deef5beb0025f1e
bbojarczuk <01034489@pw.edu.pl>
2023-03-18 19:29:12 +0100

    battlewindow.ui modified, some issues fixed

--- GryWojenne/battlewindow.cpp
+++ GryWojenne/battlewindow.cpp
@@ -1,38 +1,14 @@
 #include "battlewindow.h"
 #include "ui_battlewindow.h"
-#include <QtWidgets>
-#include <QDebug>
-#include <QLabel>
-#include <typeinfo>
 
-
-
-
-BattleWindow::BattleWindow(QWidget *parent) :
-    QMainWindow(parent),
-    ui(new Ui::BattleWindow)
+battlewindow::battlewindow(QWidget *parent) :
+    QDialog(parent),
+    ui(new Ui::battlewindow)
 {
     ui->setupUi(this);
-    scene = new QGraphicsScene(this);
-    ui->graphicsView->setScene(scene);
-    scene->setSceneRect(0,0,500,500);
-    rysuj_plansze();
-    rysuj_flagi();
-qDebug()<<"BattleWindow konstruktor";
 }
 
-BattleWindow::~BattleWindow()
+battlewindow::~battlewindow()
 {
-
     delete ui;
-
-}
-
-
-
-//Zamknięcie okna walki
-void BattleWindow::on_pushButton_clicked()
-{
-    this->deleteLater();
-
 }
--- GryWojenne/battlewindow.h
+++ GryWojenne/battlewindow.h
@@ -1,55 +1,22 @@
 #ifndef BATTLEWINDOW_H
 #define BATTLEWINDOW_H
 
-#include <QMainWindow>
-#include <QGraphicsScene>
-#include "cprzeszkoda.h"
+#include <QDialog>
 
-class CJednostka;
 namespace Ui {
-class BattleWindow;
+class battlewindow;
 }
 
-class BattleWindow : public QMainWindow
+class battlewindow : public QDialog
 {
     Q_OBJECT
 
 public:
-
-    void tworz_oddzial();
-    explicit BattleWindow(QWidget *parent = 0);
-    ~BattleWindow();
-    std::vector <QGraphicsPixmapItem*> items_vector;
-    void dodaj_jednostki(std::vector<QString> jednostki, QString frakcja);
-    QString tekst;
-    void setJednostki_rys_NATO(std::vector<CJednostka*> jednostki);
-    void setJednostki_rys_ISIS(std::vector<CJednostka*> jednostki);
-    void setPrzeszkody_rys (std::vector<CPrzeszkoda*> przeszkody_w_potyczce);
-    void on_now();
-
-private slots:
-
-
-  void on_pushButton_clicked();
-
-    void on_Fatum_button_clicked();
+    explicit battlewindow(QWidget *parent = nullptr);
+    ~battlewindow();
 
 private:
-    bool GO=false;
-    QGraphicsScene *scene;
-    Ui::BattleWindow *ui;
-
-
-
-    void rysuj_jednostki(std::vector<CJednostka *> jednostki_do_narysowania, QString frakcja);
-    void rysuj_plansze ();
-    void rysuj_flagi ();
-    void rysuj_przeszkody(std::vector<CPrzeszkoda*> przeszkody_do_narysowania);
-    void usun_jednostke (QGraphicsPixmapItem *item);
-    std::vector<CJednostka*> jednostki_rys_ISIS;
-    std::vector<CJednostka*> jednostki_rys_NATO;
-    std::vector<CPrzeszkoda*> przeszkody_rys;
+    Ui::battlewindow *ui;
 };
-
 
 #endif // BATTLEWINDOW_H
